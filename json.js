44e54ea7acbc33df22dde1d94dd12f6096c32cff
Sohagmrida15 <sohagmrida15@gmail.com>
2024-03-01 13:52:35 +0600

    json

---
+++ json.js
@@ -0,0 +1,15 @@
+// const user = {name: 'Sohag Mridha', Job: 'Web Developer'};
+// console.log(user);
+// const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+const shop = {
+    owner: 'Sohag Mridha',
+    Address: {
+        street: 'Komisonar mor',
+        City: 'Dhamrai',
+        Country: 'Bangladesh'
+    },
+    product: ['laptop', 'mic', 'monitor']
+}
+console.log(shop);
